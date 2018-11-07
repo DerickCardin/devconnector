bf7feb647e30493b57c177f75f5272e83e1e9a7b
Derick Cardin <derickc50@gmail.com>
2018-11-07 02:56:10 +0000

    Created JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://derick:derick1@ds231242.mlab.com:31242/devconnector-derickcardin'
+    mongoURI: 'mongodb://derick:derick1@ds231242.mlab.com:31242/devconnector-derickcardin',
+    secretOrKey: 'secret'
 };
