ba3e5ef6d4665d7837d3ebe30b24e37249c3947f
Derick Cardin <derickc50@gmail.com>
2018-11-07 03:23:13 +0000

    Iplemented Passport JWT authentication strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
 
 // Load User Model
 const User = require('../../models/User');
@@ -100,5 +102,36 @@ router.post('/login', (req, res) => {
 
 
 
+//@route GET api/users/current
+//@desc return current user
+//@access Private
+
+router.get('/current', passport.authenticate('jwt', { session: false}), (req, res) => {
+  res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+  });  
+});
+
+
+
+
+
+
+
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
